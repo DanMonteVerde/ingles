cfc0f4be1e40b4a15ccb2d884ca09850af2c4265
Daniel Barbosa <tvdan2064@gmail.com>
2023-04-20 21:17:15 -0300

    sucesso

--- script.js
+++ script.js
@@ -1,21 +1,37 @@
-document.addEventListener('keydown', function(event) {
+
+document.addEventListener('DOMContentLoaded', function() {
+  // Seu código JavaScript aqui
+  document.addEventListener('keydown', function(event) {
     if (event.key === 'd') {
+      var minhaDiv = document.querySelector('#minhadiv');
       var carrinho = document.querySelector('#car1');
       var posicaoAtual = parseInt(carrinho.style.marginLeft || 0, 10);
       carrinho.style.marginLeft = posicaoAtual + 40 + 'px';
+      var posicaoLinha=1240
+      if(posicaoAtual >= posicaoLinha){
+        minhaDiv.innerHTML = 'Carro Laranja ganhou!!';
+        minhaDiv.style.color="orange"
+      }
     }
+  });
+
+  document.addEventListener('keydown', function(event) {
+    if (event.key === 'l') {
+      var minhaDiv = document.querySelector('#minhadiv');
+      var carrinho = document.querySelector('#car2');
+      var posicaoAtual = parseInt(carrinho.style.marginLeft || 0, 10);
+      carrinho.style.marginLeft = posicaoAtual + 40 + 'px';
+      var posicaoLinha=1240
+      if(posicaoAtual >= posicaoLinha){
+        minhaDiv.innerHTML = 'Carro Branco ganhou!!';
+        minhaDiv.style.color="white"
+    }
+  }
+
   });
 
   document.addEventListener('keydown', function(event) {
     if (event.key === 'a') {
-      var carrinho = document.querySelector('#car2');
-      var posicaoAtual = parseInt(carrinho.style.marginLeft || 0, 10);
-      carrinho.style.marginLeft = posicaoAtual + 40 + 'px';
-    }
-  });
-
-  document.addEventListener('keydown', function(event) {
-    if (event.key === 't') {
       var carrinho = document.querySelector('#car1');
       var posicaoAtual = parseInt(carrinho.style.marginLeft || 0, 10);
       carrinho.style.marginLeft = posicaoAtual -40 + 'px';
@@ -24,11 +40,12 @@ document.addEventListener('keydown', function(event) {
   
 
   document.addEventListener('keydown', function(event) {
-    if (event.key === 'r') {
+    if (event.key === 'j') {
       var carrinho = document.querySelector('#car2');
       var posicaoAtual = parseInt(carrinho.style.marginLeft || 0, 10);
       carrinho.style.marginLeft = posicaoAtual -40 + 'px';
     }
+});
 });
 
 
