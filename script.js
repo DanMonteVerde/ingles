81bdbfe054cda82915765b6109be9f2f8e8f14f9
Daniel Barbosa <tvdan2064@gmail.com>
2023-05-01 16:09:53 -0300

    fim

--- script.js
+++ script.js
@@ -63,8 +63,8 @@ document.addEventListener('DOMContentLoaded', function() {
   };
 
   var handlerJ = function(event){
-    if (event.key === 'a') {
-      var carrinho = document.querySelector('#car1');
+    if (event.key === 'j') {
+      var carrinho = document.querySelector('#car2');
       var posicaoAtual = parseInt(carrinho.style.marginLeft || 0, 10);
       carrinho.style.marginLeft = posicaoAtual - 40 + 'px';
 
