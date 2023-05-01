9742a6d0a7042a356b320c15115d1897e15d3b85
Daniel Barbosa <tvdan2064@gmail.com>
2023-05-01 15:44:38 -0300

    up

--- script.js
+++ script.js
@@ -1,41 +1,101 @@
-
 document.addEventListener('DOMContentLoaded', function() {
-  document.addEventListener('keydown', function(event) {
-
+  var handlerD = function(event) {
     if (event.key === 'd') {
       var carrinho = document.querySelector('#car1');
       var posicaoAtual = parseInt(carrinho.style.marginLeft || 0, 10);
       carrinho.style.marginLeft = posicaoAtual + 40 + 'px';
-    }
-  });
 
-  document.addEventListener('keydown', function(event) {
+      var linha = document.querySelector('#linha');
+      var linhaPosicao = linha.getBoundingClientRect().left + window.pageXOffset;
+      var carrinhoPosicao = carrinho.getBoundingClientRect().left + window.pageXOffset;
+
+      if (carrinhoPosicao > linhaPosicao) {
+        document.querySelector('#minhadiv').innerText = "O carrinho laranja é o vencedor!";
+        document.querySelector('#minhadiv').style.color = "orange";
+        document.removeEventListener('keydown', handlerD);
+        document.removeEventListener('keydown', handlerL);
+        document.removeEventListener('keydown', handlerA);
+        document.removeEventListener('keydown', handlerJ);
+      }
+    }
+  };
+
+  var handlerL = function(event) {
     if (event.key === 'l') {
       var carrinho = document.querySelector('#car2');
       var posicaoAtual = parseInt(carrinho.style.marginLeft || 0, 10);
       carrinho.style.marginLeft = posicaoAtual + 40 + 'px';
-  }
 
-  });
+      var linha = document.querySelector('#linha');
+      var linhaPosicao = linha.getBoundingClientRect().left + window.pageXOffset;
+      var carrinhoPosicao = carrinho.getBoundingClientRect().left + window.pageXOffset;
 
-  document.addEventListener('keydown', function(event) {
+      if (carrinhoPosicao > linhaPosicao) {
+        document.querySelector('#minhadiv').innerText = "O carrinho branco é o vencedor!";
+        document.querySelector('#minhadiv').style.color = "white";
+        document.removeEventListener('keydown', handlerD);
+        document.removeEventListener('keydown', handlerL);
+        document.removeEventListener('keydown', handlerA);
+        document.removeEventListener('keydown', handlerJ);
+      }
+    }
+  };
+
+  var handlerA = function(event) {
     if (event.key === 'a') {
       var carrinho = document.querySelector('#car1');
       var posicaoAtual = parseInt(carrinho.style.marginLeft || 0, 10);
-      carrinho.style.marginLeft = posicaoAtual -40 + 'px';
-    }
-  });
-  
+      carrinho.style.marginLeft = posicaoAtual - 40 + 'px';
 
-  document.addEventListener('keydown', function(event) {
-    if (event.key === 'j') {
-      var carrinho = document.querySelector('#car2');
+      var linha = document.querySelector('#linha');
+      var linhaPosicao = linha.getBoundingClientRect().left + window.pageXOffset;
+      var carrinhoPosicao = carrinho.getBoundingClientRect().left + window.pageXOffset;
+
+      if (carrinhoPosicao > linhaPosicao) {
+        document.querySelector('#minhadiv').innerText = "O carrinho laranja é o vencedor!";
+        document.querySelector('#minhadiv').style.color = "orange";
+        document.removeEventListener('keydown', handlerD);
+        document.removeEventListener('keydown', handlerL);
+        document.removeEventListener('keydown', handlerA);
+        document.removeEventListener('keydown', handlerJ);
+      }
+    }
+  };
+
+  var handlerJ = function(event){
+    if (event.key === 'a') {
+      var carrinho = document.querySelector('#car1');
       var posicaoAtual = parseInt(carrinho.style.marginLeft || 0, 10);
-      carrinho.style.marginLeft = posicaoAtual -40 + 'px';
+      carrinho.style.marginLeft = posicaoAtual - 40 + 'px';
+
+      var linha = document.querySelector('#linha');
+      var linhaPosicao = linha.getBoundingClientRect().left + window.pageXOffset;
+      var carrinhoPosicao = carrinho.getBoundingClientRect().left + window.pageXOffset;
+
+      if (carrinhoPosicao > linhaPosicao) {
+        document.querySelector('#minhadiv').innerText = "O carrinho laranja é o vencedor!";
+        document.querySelector('#minhadiv').style.color = "orange";
+        document.removeEventListener('keydown', handlerD);
+        document.removeEventListener('keydown', handlerL);
+        document.removeEventListener('keydown', handlerA);
+        document.removeEventListener('keydown', handlerJ);
+      }
     }
+
+
+
+}
+
+
+document.addEventListener('keydown', handlerD);
+document.addEventListener('keydown', handlerL);
+document.addEventListener('keydown', handlerA);
+document.addEventListener('keydown', handlerJ);
 });
 
-});
+
+
+
 
 
 
