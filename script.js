5e52a7b4db29cfb5442ebec1eb266a82d014fd39
Daniel Barbosa <tvdan2064@gmail.com>
2023-05-01 14:00:31 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,19 @@
 
 document.addEventListener('DOMContentLoaded', function() {
-  // Seu código JavaScript aqui
   document.addEventListener('keydown', function(event) {
+
     if (event.key === 'd') {
-      var minhaDiv = document.querySelector('#minhadiv');
       var carrinho = document.querySelector('#car1');
       var posicaoAtual = parseInt(carrinho.style.marginLeft || 0, 10);
       carrinho.style.marginLeft = posicaoAtual + 40 + 'px';
-      var posicaoLinha=1240
-      if(posicaoAtual >= posicaoLinha){
-        minhaDiv.innerHTML = 'Carro Laranja ganhou!!';
-        minhaDiv.style.color="orange"
-      }
     }
   });
 
   document.addEventListener('keydown', function(event) {
     if (event.key === 'l') {
-      var minhaDiv = document.querySelector('#minhadiv');
       var carrinho = document.querySelector('#car2');
       var posicaoAtual = parseInt(carrinho.style.marginLeft || 0, 10);
       carrinho.style.marginLeft = posicaoAtual + 40 + 'px';
-      var posicaoLinha=1240
-      if(posicaoAtual >= posicaoLinha){
-        minhaDiv.innerHTML = 'Carro Branco ganhou!!';
-        minhaDiv.style.color="white"
-    }
   }
 
   });
@@ -46,6 +34,7 @@ document.addEventListener('DOMContentLoaded', function() {
       carrinho.style.marginLeft = posicaoAtual -40 + 'px';
     }
 });
+
 });
 
 
